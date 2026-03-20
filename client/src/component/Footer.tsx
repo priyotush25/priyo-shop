2d6d5e25e96db7e9863daadcd893df469fdc3436
Priyotush <priyotushpaul40@gmail.com>
2026-03-20 18:29:41 +0600

    footer section add

--- client/src/component/Footer.tsx
+++ client/src/component/Footer.tsx
@@ -1,8 +1,46 @@
+import { Copyright } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
     return (
-        <div>
-            footer
+        <div className="mt-16 flex flex-col items-center gap-4 md:gap-0 md:flex-row md:items-start md:justify-between p-8 rounded-lg bg-gray-800 text-white">
+            <div className="flex flex-col gap-4">
+                <Link href="/" className="flex items-center">
+                    <Image src="/logo.png"
+                        alt="PriyoShop"
+                        width={36}
+                        height={36}
+                        className="w-6 h-6 md:w-9 md:h-9" />
+                    <p className="text-md font-medium">PriyoShop</p>
+                </Link>
+                <p className="text-sm text-gray-400 flex items-center"> <Copyright className="w-3 h-3 mr-2" /> 2026 PriyoShop </p>
+                <p className="text-sm text-gray-400">All right reserved.</p>
+            </div>
+
+            <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+                <p className="text-amber-50">Links</p>
+                <Link href="/">Home</Link>
+                <Link href="/">Contact</Link>
+                <Link href="/">Terms of Service</Link>
+                <Link href="/">Privacy Policy</Link>
+            </div>
+
+            <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+                <p className="text-amber-50">Links</p>
+                <Link href="/">All Products</Link>
+                <Link href="/">New Arrivals</Link>
+                <Link href="/">Best Sellers</Link>
+                <Link href="/">Sale</Link>
+            </div>
+
+            <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+                <p className="text-amber-50">Links</p>
+                <Link href="/">About</Link>
+                <Link href="/">Contact</Link>
+                <Link href="/">Blog</Link>
+                <Link href="/">Affiliate Program</Link>
+            </div>
         </div>
     );
 };
